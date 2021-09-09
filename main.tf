282c1896d4be2d261a0266265b96b8a288ea8888
lijogeorge02 <lijogeorge02@gmail.com>
2021-09-09 23:58:14 +0530

    added tags for instance

--- main.tf
+++ main.tf
@@ -33,6 +33,10 @@ resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
+   
+  tags = {
+     Name = "web-server-1"
+     }
 
   user_data = <<-EOF
               #!/bin/bash
